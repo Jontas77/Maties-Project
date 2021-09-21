1d404ac793e72a0d8fd495ac819f020ee4898a7f
Jonathan Heugh <mrjbheugh@gmail.com>
2021-09-21 16:40:28 +0200

    Build dashboard

--- client/src/pages/dashboards/StudentDashboard.js
+++ client/src/pages/dashboards/StudentDashboard.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import logo from "../stellenbosch-university-logo.png";
 import { toast } from "react-toastify";
+import "./Dashboard.css";
 
 const StudentDashboard = ({ setAuth }) => {
 	const [name, setName] = useState("");
@@ -32,10 +34,25 @@ const StudentDashboard = ({ setAuth }) => {
 	};
 
 	return (
-		<div>
-			<h1>Welcome {name}</h1>
-			<h2> Student Dashboard</h2>
-			<button className="btn btn-primary" onClick={(e) => logout(e)}>Logout</button>
+		<div className="container">
+			<div className="header-container">
+				<div className="logo">
+					<img src={logo} className="logo" alt="Stellies Logo" />
+				</div>
+				<div className="introduction">
+					<h1>Welcome {name}</h1>
+					<h2> Student Dashboard</h2>
+				</div>
+				<div>
+					<button className="btn btn-primary profile">Profile</button>
+					<button className="btn btn-primary logout" onClick={(e) => logout(e)}>
+						Logout
+					</button>
+				</div>
+			</div>
+			<div className="display">
+				--Nothing to Display--
+			</div>
 		</div>
 	);
 };
